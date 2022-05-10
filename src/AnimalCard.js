8e3c0cb6a1495a47c30949e7d00cd4dc24e7a9a9
Skolotajs SK. Skolotajs <skolotajs@adazuvsk.lv>
2022-05-10 16:09:25 +0300

    Sakotnejas failu izmainas

---
+++ src/AnimalCard.js
@@ -0,0 +1,25 @@
+import { Text, Card, Image, Badge, Group } from "@mantine/core";
+
+function AnimalCard({ dati }) {
+  return (
+    <Card shadow="sm" p="lg">
+      <Card.Section>
+        <Image src={dati.image} height={160} alt={dati.name} />
+      </Card.Section>
+      <Group position="apart" style={{ marginBottom: 5, marginTop: 5 }}>
+        <Text weight={500}>{dati.name}</Text>
+        <Badge color="pink" variant="light">
+          {dati.species}
+        </Badge>
+        <Badge color="green" variant="light">
+          {dati.age} gadi
+        </Badge>
+      </Group>
+      <Text size="sm" style={{ lineHeight: 1.5 }}>
+        {dati.description}
+      </Text>
+    </Card>
+  );
+}
+
+export default AnimalCard;
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { SimpleGrid, Container } from "@mantine/core";
+
+import AnimalCard from "./AnimalCard";
+
+const dati = [
+  {
+    name: "Džeris",
+    species: "Suns",
+    age: 5,
+    description: "Džeris ir draudzīgs. Būs piemērots ģimenēm ar bērniem.",
+    image:
+      "https://media.istockphoto.com/photos/happy-adopted-dog-playing-in-the-garden-picture-id1138793510?k=20&m=1138793510&s=612x612&w=0&h=ZnFjIksvjNWwMTD0H4fH24h4eqEMb1TvGZRGG5acUzg=",
+  },
+  {
+    name: "Muris",
+    species: "Kaķis",
+    age: 7,
+    description:
+      "Murim patīk sildīties pie kamīna, tomēr vēl vairāk viņš novērtē cilvēka sirds siltumu.",
+    image:
+      "https://images.unsplash.com/photo-1511044568932-338cba0ad803?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60",
+  },
+];
+
+function App() {
+  const allAnimalsRendered = dati.map((animal) => (
+    <AnimalCard dati={animal} key={animal.name} />
+  ));
+
+  return (
+    <Container>
+      <SimpleGrid cols={3}>{allAnimalsRendered}</SimpleGrid>
+    </Container>
+  );
+}
+
+export default App;
